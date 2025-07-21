041193a88538e10ada4deb1c41d3dd3a531203be
MulyaATS <mulyaats@gmail.com>
2025-07-21 15:27:30 +0530

    Add files via upload

---
+++ src/main/java/com/mulya/globaluser/GlobaluserApplication.java
@@ -0,0 +1,13 @@
+package com.mulya.globaluser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobaluserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobaluserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mulya/globaluser/GlobaluserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mulya.globaluser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobaluserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
